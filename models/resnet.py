f8ce68bc88b61923b24ab23f8bc78fb46371055e
unknown <jaeheon.dev@gmail.com>
2021-06-03 18:44:40 +0900

    Implement BasicBlock class in resnet.py file

--- models/resnet.py
+++ models/resnet.py
@@ -1,11 +1,38 @@
 import torch
 import torch.nn as nn
-import torch.function as f
+import torch.functional as f
 
 
-class shortcut:
-    def __init__(self, in_planes, out_planes, )
+class BasicBlock(nn.Module):
 
-class Resnet:
-    pass
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+
+        self.expansion = 1   
+
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcurt = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcurt = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+    '''
+    1x1 -> 3x3 -> 1x1 
+    '''
+class ResNet(nn.Module):
+    def __init__(self, block, num_blokcs, num_classes=10):
+        super(ResNet, self).__init__()
+    
     
