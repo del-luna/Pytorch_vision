ed080da132ff5879bf164154185f269f19ce4af4
unknown <jaeheon.dev@gmail.com>
2021-06-04 22:59:02 +0900

    upload Bottleneck class

--- models/resnet.py
+++ models/resnet.py
@@ -26,11 +26,27 @@ class BasicBlock(nn.Module):
             )
 
 class Bottleneck(nn.Module):
+    expansion = 4
+
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
-    '''
-    1x1 -> 3x3 -> 1x1 
-    '''
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
 class ResNet(nn.Module):
     def __init__(self, block, num_blokcs, num_classes=10):
         super(ResNet, self).__init__()
